f6ae7a01fbf4bcb3ce5d5b829f7fbc0ce86ee158
Mai Nong (Mia) <85831304+MiaHenstridge@users.noreply.github.com>
2024-08-13 13:44:13 +0700

    Update day mode background

--- script.js
+++ script.js
@@ -1,54 +1,199 @@
 "use-strict";
-function createHeart() {
-    const heart = document.createElement('div');
-    heart.classList.add('heart');
-    heart.style.left = Math.random() * 100 + 'vw';
-    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
-//    heart.style.zIndex = "-1";
-    heart.innerText = '\u2764';
-    document.body.appendChild(heart);
-    setTimeout(() => {
-        heart.remove();
-    }, 5000);
-}
-
-let heartRain;
+//function createHeart() {
+//    const heart = document.createElement('div');
+//    heart.classList.add('heart');
+//    heart.style.left = Math.random() * 100 + 'vw';
+//    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+////    heart.style.zIndex = "-1";
+//    heart.innerText = '\u2764';
+//    document.body.appendChild(heart);
+//    setTimeout(() => {
+//        heart.remove();
+//    }, 5000);
+//}
+//
+//let heartRain;
 
 // Day mode background
 function start_day_mode() {
-    // Canvas and settings
-    var canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext('2d'),
-        width = canvas.width = window.innerWidth,
-        height = canvas.height = window.innerHeight,
-        paused = false;                                 // keep this set to false all the time so that shooting stars keep going
+    var LeafScene = function(el) {
+        this.viewport = el;
+        this.world = document.createElement('div');
+        this.leaves = [];
 
-    function updateDaySky() {
-        if (!paused) {
-            ctx.clearRect(0, 0, width, height);
-            // Create a linear gradient
-            const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+        this.options = {
+            numLeaves: 20,
+            wind: {
+                magnitude: 1.2,
+                maxSpeed: 12,
+                duration: 300,
+                start: 0,
+                speed: 0,
+            },
+        };
 
-            // Add color stops
-            gradient.addColorStop(0, '#8DD0FC'); // Top color #360654
-            gradient.addColorStop(1, '#DDB4F6'); // Bottom color #17636A
+        this.width = this.viewport.offsetWidth;
+        this.height = this.viewport.offsetHeight;
 
-            // Use the gradient as the fill style
-            ctx.fillStyle = gradient;
+        // animation helper
+        this.timer = 0;
 
-            ctx.fillRect(0, 0, width, height);
-            ctx.fill();
+        this._resetLeaf = function(leaf) {
+            // place leaf towards top left
+            leaf.x = this.width * 2 - Math.random() * this.width * 1.75;
+            leaf.y = -10;
+            leaf.z = Math.random() * 200;
+
+            if (leaf.x > this.width) {
+                leaf.x = this.width + 10;
+                leaf.y = Math.random() * this.height / 2;
+            }
+
+            // at the start, leaf can be anywhere
+            if (this.timer == 0) {
+                leaf.y = Math.random() * this.height;
+            }
+
+            // Choose axis of rotation
+            // If axis is not x, choose a random static x-rotation for greater variability
+            leaf.rotation.speed = Math.random() * 10;
+            var randomAxis = Math.random();
+            if (randomAxis > 0.5) {
+                leaf.rotation.axis = 'X';
+            } else if (randomAxis > 0.25) {
+                leaf.rotation.axis = 'Y';
+                leaf.rotation.x = Math.random() * 180 + 90;
+            } else {
+                leaf.rotation.axis = 'Z';
+                leaf.rotation.x = Math.random() * 360 - 180;
+                //looks weird if the rotation is too fast around this axis
+                leaf.rotation.speed = Math.random() * 3;
+            }
+
+            // random speed
+            leaf.xSpeedVariation = Math.random() * 0.8 - 0.4;
+            leaf.ySpeed = Math.random() + 1.5;
+
+            return leaf;
         }
-        requestAnimationFrame(updateDaySky);
+        this._updateLeaf = function(leaf) {
+            var leafWindSpeed = this.options.wind.speed(this.timer - this.options.wind.start, leaf.y);
+            var xSpeed = leafWindSpeed + leaf.xSpeedVariation;
+            leaf.x -= xSpeed;
+            leaf.y += leaf.ySpeed;
+            leaf.rotation.value += leaf.rotation.speed;
+
+            var t = 'translateX( ' + leaf.x + 'px ) translateY( ' + leaf.y + 'px ) translateZ( ' + leaf.z + 'px )  rotate' + leaf.rotation.axis + '( ' + leaf.rotation.value + 'deg )';
+
+            if (leaf.rotation.axis !== 'X') {
+                t += ' rotateX(' + leaf.rotation.x + 'deg)';
+            }
+            leaf.el.style.webkitTransform = t;
+            leaf.el.style.MozTransform = t;
+            leaf.el.style.oTransform = t;
+            leaf.el.style.transform = t;
+
+            // reset if out of view
+            if (leaf.x < -10 || leaf.y > this.height + 10) {
+                this._resetLeaf(leaf);
+            }
+        }
+
+        this._updateWind = function() {
+            // wind follows a sine curve: asin(b*time + c) + a
+            // where a = wind magnitude as a function of leaf position, b = wind.duration, c = offset
+            // wind duration should be related to wind magnitude, e.g. higher windspeed -> longer gust duration
+
+            if (this.timer === 0 || this.timer > (this.options.wind.start + this.options.wind.duration)) {
+                this.options.wind.magnitude = Math.random() * this.options.wind.maxSpeed;
+                this.options.wind.duration = this.options.wind.magnitude * 50 + (Math.random() * 20 - 10);
+                this.options.wind.start = this.timer;
+
+                var screenHeight = this.height;
+
+                this.options.wind.speed = function(t, y) {
+                    // should go from full wind speed at the top, to 1/2 speed at the bottom, using leaf Y
+                    var a = this.magnitude/2 * (screenHeight - 2*y/3) / screenHeight;
+                    return a * Math.sin(2*Math.PI/this.duration * t + (3 * Math.PI/2)) + a;
+                }
+            }
+        }
     }
 
-    // Run
-    updateDaySky();
-    heartRain = setInterval(createHeart, 300);
+    LeafScene.prototype.init = function() {
+        for (var i = 0; i < this.options.numLeaves; i++) {
+            var leaf = {
+                el: document.createElement('div'),
+                x: 0,
+                y: 0,
+                z: 0,
+                rotation: {
+                    axis: 'X',
+                    value: 0,
+                    speed: 0,
+                    x: 0
+                },
+                xSpeedVariation: 0,
+                ySpeed: 0,
+                path: {
+                    type: 1,
+                    start: 0
+                },
+                image: 1
+            };
+            this._resetLeaf(leaf);
+            this.leaves.push(leaf);
+            this.world.appendChild(leaf.el);
+        }
+        this.world.className = 'leaf-scene';
+        this.viewport.appendChild(this.world);
 
-    window.onfocus = function() {
-        paused = false;
+        //set perspective
+        this.world.style.webkitPerspective = '400px';
+        this.world.style.MozPerspective = '400px';
+        this.world.style.oPerspective = '400px';
+        this.world.style.perspective = '400px';
+
+        // reset window height/width on resize
+        var self = this;
+
+        window.onresize = function(event) {
+            self.width = self.viewport.offsetWidth;
+            self.height = self.viewport.offsetHeight;
+        };
     }
+
+    LeafScene.prototype.render = function() {
+        this._updateWind();
+        for (var i = 0; i < this.leaves.length; i++) {
+            this._updateLeaf(this.leaves[i]);
+        }
+        this.timer++;
+
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    // hide canvas
+    var canvas = document.querySelector('canvas');
+    canvas.style.display = 'none';
+
+
+    // Create a new <div> element
+    var fallingLeavesDiv = document.createElement('div');
+
+    // Set the class name to 'falling-leaves'
+    fallingLeavesDiv.className = 'falling-leaves';
+
+    // Append the <div> to the body
+    document.body.appendChild(fallingLeavesDiv);
+
+    // Start up leaf scene
+    var leafContainer = document.querySelector('.falling-leaves'),
+        leaves = new LeafScene(leafContainer);
+
+
+    leaves.init();
+    leaves.render();
 }
 
 // night mode background
@@ -139,24 +284,33 @@ function start_night_mode() {
         shootingStarRadius = 3,
         paused = false;                                 // keep this set to false all the time so that shooting stars keep going
 
-//    window.addEventListener('resize', () => {
-//        var canvas = document.getElementById('canvas'),
-//        ctx = canvas.getContext('2d'),
-//        width = canvas.width = window.innerWidth,
-//        height = canvas.height = window.innerHeight;
-//    });
+    canvas.style.display = 'block';
+
+    // Resize canvas function
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        updateShootingStar();  // Redraw content if necessary
+    }
+
+
 
     // Create all stars
-    for (var j = 0; j < layers.length; j += 1) {
-        var layer = layers[j];
-        for (var i = 0; i < layer.count; i += 1) {
-            var star = particle.create(randomRange(0, width), randomRange(0, height), 0, 0);        // create a new star at a random position x, y and speed = 0, direction = 0
-            star.radius = starBaseRadius * layer.scale;                                             // set star radius according to its layer's scale
-            star.setSpeed(layer.speed);                                                             // set star speed according to its layer's speed
-            star.setHeading(degreesToRads(starsAngle));                                             // set star heading according to starsAngle
-            stars.push(star);
+    function createStar() {
+        for (var j = 0; j < layers.length; j += 1) {
+            var layer = layers[j];
+            for (var i = 0; i < layer.count; i += 1) {
+                var star = particle.create(randomRange(0, width), randomRange(0, height), 0, 0);        // create a new star at a random position x, y and speed = 0, direction = 0
+                star.radius = starBaseRadius * layer.scale;                                             // set star radius according to its layer's scale
+                star.setSpeed(layer.speed);                                                             // set star speed according to its layer's speed
+                star.setHeading(degreesToRads(starsAngle));                                             // set star heading according to starsAngle
+                stars.push(star);
+            }
         }
     }
+
+    createStar();
+
 
     function createShootingStar() {
         var shootingStar = particle.create(randomRange(width / 2, width), randomRange(0, height / 2), 0, 0);
@@ -322,9 +476,9 @@ function enableDarkMode(){
     themeToggle.setAttribute("data-attribute", "dark");
 
     // Clear heart rain effect
-    clearInterval(heartRain);
-    document.querySelectorAll('.heart').forEach(heart => heart.remove());
-
+//    clearInterval(heartRain);
+//    document.querySelectorAll('.heart').forEach(heart => heart.remove());
+    document.querySelectorAll('.falling-leaves').forEach(d => d.remove());
     setTimeout(start_night_mode, 100);
 };
 
